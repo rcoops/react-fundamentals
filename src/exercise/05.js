102270093b272a8a0565c85fa2094d54d2055d45
Rick Cooper <rick.cooper@upside.energy>
2020-12-03 20:26:20 +0000

    feat: 5.2

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,13 +4,18 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-const Box = ({ className = '', style, ...rest }) => (
-  <div className={`box ${className}`.trim()} style={{fontStyle: 'italic', ...style}} {...rest} />
-);
+const Box = ({ size , style, ...rest }) => {
+  return(
+    <div
+      className={`box ${['small', 'medium', 'large'].includes(size) ?  `box--${size}` : ''}`.trim()}
+      style={{fontStyle: 'italic', ...style}} {...rest}
+    />
+  )
+};
 
-const smallBox = <Box className="box--small" style={{ backgroundColor: 'lightblue' }}>small lightblue box</Box>
-const mediumBox = <Box className="box--medium" style={{ backgroundColor: 'pink' }}>medium pink box</Box>
-const largeBox = <Box className="box--large" style={{ backgroundColor: 'orange' }}>large orange box</Box>
+const smallBox = <Box size="small" style={{ backgroundColor: 'lightblue' }}>small lightblue box</Box>
+const mediumBox = <Box size="medium" style={{ backgroundColor: 'pink' }}>medium pink box</Box>
+const largeBox = <Box size="large" style={{ backgroundColor: 'orange' }}>large orange box</Box>
 
 function App() {
   return (
