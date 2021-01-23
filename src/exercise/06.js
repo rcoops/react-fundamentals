b198d098cd9031993abae7123443c18d1085dc60
Rick Cooper <rick.cooper@upside.energy>
2021-01-23 15:15:22 +0000

    feat: 6.2 validation

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,21 +1,28 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = useRef();
+  const [error, setError] = useState(null);
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmitUsername(inputRef.current.value)
   };
+
+  const onChange = ({target: { value }}) => {
+    const isValid = value === value?.toLowerCase();
+    setError(isValid ? null : 'Username must be lower case');
+  }
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" ref={inputRef} />
+        <input id="usernameInput" type="text" ref={inputRef} onChange={onChange} />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={!!error}>Submit</button>
+      { !!error && <div role="alert" style={{color: '#AA0000'}}>{error}</div> }
     </form>
   )
 }
